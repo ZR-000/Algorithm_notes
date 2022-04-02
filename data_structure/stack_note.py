a66b63ccdaf2ac3952474949598ae98c08339989
ZR-000 <3477037949@qq.com>
2022-04-02 20:39:49 +0800

    stack

---
+++ data_structure/stack_note.py
@@ -0,0 +1,28 @@
+# 突出特点：后进先出，插入和删除均在栈顶进行，有一个顶指针top用于指示栈顶位置
+
+# 使用数组实现，数组末端即栈顶
+
+class Stack():
+    def __init__(self) -> None:
+        self.list=[]
+
+    def length(self):
+        return len(self.list)
+    
+    def isempty(self):
+        return self.list==[]
+    def push(self.d):
+        self.list.append(d)
+    def gettop(self):
+        if self.isempty():
+            return 'Stack is empty'
+        else:
+            return self.list[-1]
+    def pop(self):
+        if self.isempty():
+            return 'Stack is empty'
+        else:
+            return self.list.pop()
+    
+
+
