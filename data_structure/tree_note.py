df3d48dd6c09a44724cf29318e537873f420f88e
ZR-000 <3477037949@qq.com>
2022-04-02 21:28:01 +0800

    tree but not comletely true

--- data_structure/tree_note.py
+++ data_structure/tree_note.py
@@ -8,3 +8,51 @@
 ## 任意二叉树，叶子节点数n0，度为2的节点为n2，则n0=n2+1
 
 ## 常见二叉树：满二叉树、完全二叉树、平衡二叉树、搜索二叉树
+
+# 二叉树的实现
+# 二叉树的每个节点都是node类对象，通过Tree类中的add()方法逐个项树中加入节点
+
+from platform import node
+from re import T
+
+
+class Node():
+    def __init__(self,val=None,left=None,right=None) -> None:
+        '''
+        节点值和节点指针域（左右指针）
+        '''
+        self.value=val
+        self.left=left
+        self.right=right
+
+class Tree():
+    def __init__(self,node=None) -> None:
+        #根节点
+        self.root=node
+
+    def add_node(self,item=None):
+        node=Node(val=item)
+        if not self.root:
+            self.root=node
+        else:
+            #各个节点完成一次存储FIFO对每个节点检查并添加子树（节点）
+            queue=[]
+            print(queue)
+            queue.append(self.root)
+            while True:
+                current_exist_node=queue.pop(0)
+                if current_exist_node.value is None:
+                    #检查空值节点，不存储
+                    continue
+                if not current_exist_node.left:
+                    current_exist_node.left=node
+                    return
+                elif not current_exist_node.right:
+                    current_exist_node.right=node
+                    return
+                else:
+                    queue.append(current_exist_node.left)
+                    queue.append(current_exist_node.right)
+tree=Tree()
+tree.add_node(1)
+tree.add_node(2)
